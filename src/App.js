79d40ddda0ad3ab086fce7c18b70b4d2d63b843c
DhanveerSingh2002 <dhanveersingh124925@gmail.com>
2023-12-23 15:53:14 +0530

    Navbar updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <nav className="py-5 px-5 bg-purple-900 navbar">
-      <ul className="inline-flex  ull">
-        <li className="name font-bold ml-0 mr-4 text-3xl cursor-pointer inline-block italic">Textify</li>
-        <li className="ml-5 mr-4 pt-1  inline-block cursor-pointer text-2xl">Home</li>
-        <li className="mx-4 pt-1 cursor-pointer inline-block text-2xl">About</li>
-        <li className="mx-4 pt-1 cursor-pointer inline-block text-2xl">Socials</li>
-        
-      </ul>
-    </nav>
+      <Navbar/>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../App.css';
+
+const Navbar = () => {
+  return (
+    <div>
+      <>
+    <nav className="py-5 px-5 bg-purple-900 navbar">
+      <ul className="inline-flex  ull">
+        <li className="name font-bold ml-0 mr-4 text-3xl cursor-pointer inline-block italic">Textify</li>
+        <li className="home ml-5 mr-4 pt-1  inline-block cursor-pointer text-2xl">Home</li>
+        <li className="about mx-4 pt-1 cursor-pointer inline-block text-2xl">About</li>
+        <li className="socials mx-4 pt-1 cursor-pointer inline-block text-2xl">Socials</li>
+      </ul>
+    </nav>
+    </>
+    </div>
+  )
+}
+
+export default Navbar
