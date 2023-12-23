44f9865333d222577b88bf0ee913796099148c2e
DhanveerSingh2002 <dhanveersingh124925@gmail.com>
2023-12-23 15:27:50 +0530

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav className="py-5 px-5 bg-purple-900 navbar">
+      <ul className="inline-flex  ull">
+        <li className="name font-bold ml-0 mr-4 text-3xl cursor-pointer inline-block italic">Textify</li>
+        <li className="ml-5 mr-4 pt-1  inline-block cursor-pointer text-2xl">Home</li>
+        <li className="mx-4 pt-1 cursor-pointer inline-block text-2xl">About</li>
+        <li className="mx-4 pt-1 cursor-pointer inline-block text-2xl">Socials</li>
+        
+      </ul>
+    </nav>
+    </>
   );
 }
 
