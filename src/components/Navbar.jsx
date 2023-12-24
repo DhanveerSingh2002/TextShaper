a5ac51c878f87c7536041fb8d98035996b2f2d9f
DhanveerSingh2002 <dhanveersingh124925@gmail.com>
2023-12-24 17:54:34 +0530

    Home page done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
       <>
-    <nav className="py-5 px-5 bg-purple-900 navbar">
+    <nav className="py-4 px-5 bg-purple-900 navbar">
       <ul className="inline-flex  ull">
         <li className="name font-bold ml-0 mr-4 text-3xl cursor-pointer inline-block italic">Textify</li>
         <li className="home ml-5 mr-4 pt-1  inline-block cursor-pointer text-2xl">Home</li>
--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -3,13 +3,15 @@ import "../App.css"
 
 const Textform = () => {
 
-  const[text, setText]=useState();
+  const[text, setText]=useState("");
   const [isBold, setIsBold] = useState(false);
   const [isItalic, setIsItalic] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
 
   const HandleChange = (event) => {
     setText(event.target.value);
+    updateWordCount(event.target.value);
   }
 
   const HandleUC = () => {
@@ -28,6 +30,23 @@ const Textform = () => {
     setIsItalic(!isItalic);
   };
 
+  const HandleClear = () => {
+    setText("");
+    setWordCount(0);
+  };
+
+  const HandleSpeak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  }
+
+  const updateWordCount = (newText) => {
+    const count = newText.trim() === '' ? 0 : newText.split(" ").filter(word => word !== '').length;
+    setWordCount(count);
+  };
+
+
   return (
     <>
     <div class="mt-6">
@@ -42,10 +61,18 @@ const Textform = () => {
         </textarea>
         <br/>
     </div>
-    <button className="btn btn-primary UC" onClick={HandleUC}>Convert to Uppercase</button>
-    <button className="btn btn-primary LC" onClick={HandleLC}>Convert to Lowercase</button>
-    <button className="btn btn-primary bold" onClick={HandleBold}>{isBold ? 'Unbold Text' : 'Bold Text'}</button>
-    <button className="btn btn-primary Italic" onClick={HandleItalic}>{isItalic ? 'Remove Italics' : 'Italicize Text'}</button>
+    <div>
+      <button className="btn btn-primary UC" onClick={HandleUC}>Convert to Uppercase</button>
+      <button className="btn btn-primary LC" onClick={HandleLC}>Convert to Lowercase</button>
+      <button className="btn btn-primary bold" onClick={HandleBold}>{isBold ? 'Unbold Text' : 'Bold Text' }</button>
+      <button className="btn btn-primary Italic" onClick={HandleItalic}>{isItalic ? 'Remove Italics' : 'Italicize Text'}</button>
+      <button className="btn btn-primary LC" onClick={HandleClear}>Clear Text</button>
+      <button className="btn btn-primary LC" onClick={HandleSpeak}>Speak</button>
+    </div>
+    <div>
+      <h3 className='center text-2xl italic my-6'>Total words: </h3>
+      <p className='center words text-xl'>{wordCount}</p>
+    </div>
     </>
   )
 }
